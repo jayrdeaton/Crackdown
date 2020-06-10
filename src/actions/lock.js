2b3e759a6a193946cd5bf8cc85350ab06da6deb2
jayrdeaton <jayrdeaton@gmail.com>
2020-06-10 16:36:38 -0500

    got lock and unlock working

--- src/actions/lock.js
+++ src/actions/lock.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  console.log('lock')
+const { existsSync, mkdirSync, unlinkSync, writeFileSync } = require('fs'),
+  { homedir } = require('os'),
+  { join } = require('path'),
+  cosmetic = require('cosmetic'),
+  dir = join(homedir(), '.gatekeeper')
+
+module.exports = ({ filename }) => {
+  // check dir
+  if (!existsSync(dir)) mkdirSync(dir)
+  // check path
+  const path = join(dir, filename || 'lock')
+  if (existsSync(path)) {
+    console.log(`${cosmetic.magenta('Gatekeeper')} ${filename ? filename + ' ' : ''}locked`)
+    process.exit(1)
+  } else {
+    writeFileSync(path, '')
+  }
 }
--- src/actions/unlock.js
+++ src/actions/unlock.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-  console.log('unlock')
+const { existsSync, unlinkSync } = require('fs'),
+  { homedir } = require('os'),
+  { join } = require('path'),
+  dir = join(homedir(), '.gatekeeper')
+
+module.exports = ({ filename }) => {
+  // check path
+  const path = join(dir, filename || 'lock')
+  if (existsSync(path)) unlinkSync(path)
 }
